8b84e70bc20a1b554f7607f1a7c37e43c69c5949
Jocelyn Colella <jcolella.jc@gmail.com>
2020-04-18 10:07:28 -0600

    Step1_findVarSites.sh

---
+++ Step1_findVarSites.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=shared,macmanes
+#SBATCH --job-name=var_%j
+#SBATCH --output=var_%j.log
+#SBATCH --mem=100GB #MB
+#SBATCH --exclude node117,node118
+
+### Call variable sites in ANGSD with pvalue quality threshold
+    ### To run pi/thetas downstream, we also need these options: -bam bam.filelist -doSaf 1 -anc chimpHg19.fa -GL 1
+    ### NOT folded
+
+#For Peromyscus crinitus (repeat for all species of interest)
+angsd -b pecr_bamfiles.txt -anc pecr10X_c2_HiC_chrAll_sortRC.fasta -out angsd_pecr_var \
+    -P 24 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -setMinDepth 20 -minInd 5 -minMaf 0.01 -GL 1 -doMaf 1 -doMajorMinor 1 -doGlf 2 -doPost 1 -doGeno 32 -doCounts 1 -doDepth 1 -dumpCounts 1 -doSaf 1
+
+#OUTPUTS angsd_*spp*_var
+  #Contains VARIABLES sites only
