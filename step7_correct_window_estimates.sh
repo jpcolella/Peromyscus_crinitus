6fb234b281a07924cb96a90bd78b38778b6f2255
Jocelyn Colella <jcolella.jc@gmail.com>
2020-05-29 08:08:38 -0600

    Update step7_correct_window_estimates.sh

--- step7_correct_window_estimates.sh
+++ step7_correct_window_estimates.sh
@@ -43,25 +43,25 @@ for CHR in `cat chromosomes.txt`; do
     grep "$CHR" genome_windows_${WIN}k.bed > genome_windows_${WIN}k_${CHR}.bed
 
     ### Calculate the number of sites in each window for each chromosome
-    bedtools coverage -a genome_windows_${WIN}k_${CHR}.bed -b pecr_allvar.sites_${CHR}.bed -counts > allsites_${WIN}kbwin_${CHR}.txt
+    bedtools coverage -a genome_windows_${WIN}k_${CHR}.bed -b pecr_allvar.sites_${CHR}.bed -counts > pecr_allsites_${WIN}kbwin_${CHR}.txt
         # Adds a column of coverage/depth
 
     ### Extract the 4th field and replace 0's with NA's 
-    cut -f4 allsites_${WIN}kbwin_${CHR}.txt | sed 's/^0/NA/g' > allsites_${WIN}kwin_NA_${CHR}.txt
+    cut -f4 pecr_allsites_${WIN}kbwin_${CHR}.txt | sed 's/^0/NA/g' > pecr_allsites_${WIN}kwin_NA_${CHR}.txt
 
     ### Split the per site theta file
     grep "$CHR" pi_pecr_global.bed > pi_pecr_global_${CHR}.bed
     
     ### e to the X with X being the values in col4 (e.g., Watterson's theta estimates per site)
-    awk '{print exp($4)}' pi_pecr_global_${CHR}.bed | paste pi_pecr_global_${CHR}.bed - > pi_pecr_global_log_${CHR}.bed
+    awk '{print exp($4)}' pi_pecr_global_${CHR}.bed | paste pi_pecr_global_${CHR}.bed - > pi_pecr_global_log_${CHR}.bed   
     bedtools map -a genome_windows_${WIN}k_${CHR}.bed -b pi_pecr_global_log_${CHR}.bed -c 5 -o sum | sed 's/\t[.]/\tNA/g' - > pi_pecr_global_log_${WIN}kbwin_${CHR}.txt
         # Values from B (col 5 -> exp(Watterson)) mapped onto intervales in A and removing NAs
         # 5th column is a the number of variable sites
 
     ### If it's an NA do nothing, otherwise divide col_4/col_5 (that is the sum of exp(Wattersons) divided by the total number of variant sites within the interval)
     ### to get an average value of Watterson's (measure of nucleotide diversity equivalent to pi) for a given window
-    paste pi_pecr_global_log_${WIN}kbwin_${CHR}.txt allsites_${WIN}kwin_NA_${CHR}.txt | sed 's/[.]\t/NA\t/g' - > pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt
-    awk '{if(/NA/)var="NA";else var=$4/$5;print var}' pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt | paste pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt - > pi_pecr_global_log_${WIN}kbwin_sites_corrected_${CHR}.txt
+    paste pi_pecr_global_log_${WIN}kbwin_${CHR}.txt pecr_allsites_${WIN}kwin_NA_${CHR}.txt | sed 's/[.]\t/NA\t/g' - > pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt
+    awk '{if(/NA/)var="NA";else var=$5/$4;print var}' pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt | paste pi_pecr_global_log_${WIN}kbwin_sites_${CHR}.txt - > pi_pecr_global_log_${WIN}kbwin_sites_corrected_${CHR}.txt
 
 done
 
