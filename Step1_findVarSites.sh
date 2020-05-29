5925bfa8522f7f889728131852ec97f5c5bb93e3
Jocelyn Colella <jcolella.jc@gmail.com>
2020-05-29 08:01:28 -0600

    Update Step1_findVarSites.sh

--- Step1_findVarSites.sh
+++ Step1_findVarSites.sh
@@ -11,7 +11,7 @@
 
 #For Peromyscus crinitus (repeat for all species of interest)
 angsd -b pecr_bamfiles.txt -anc pecr10X_c2_HiC_chrAll_sortRC.fasta -out angsd_pecr_var \
-    -P 24 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -setMinDepth 20 -minInd 5 -GL 1 -doMaf 1 -doMajorMinor 1 -doGlf 2 -doPost 1 -doCounts 1 -doDepth 1 -dumpCounts 1 -doSaf 1 -fold 1
+    -P 24 -SNP_pval 1e-6 -minMapQ 20 -minQ 20 -setMinDepth 20 -minInd 5 -GL 1 -doMaf 1 -doMajorMinor 1 -doGlf 2 -doPost 1 -doCounts 1 -doDepth 1 -dumpCounts 1 -doSaf 1
 
 #OUTPUTS angsd_*spp*_var
   #Contains VARIABLES sites only
