7fb9c3d88b75d781fc31fcec4df93d94df09e48f
Jocelyn Colella <jcolella.jc@gmail.com>
2020-05-29 08:05:47 -0600

    Update step6_TajDWindows.sh

--- step6_TajDWindows.sh
+++ step6_TajDWindows.sh
@@ -3,5 +3,6 @@
 #SBATCH --output=S6_%j.log
 
 STEP 6: Do a NON-OVERLAPPING sliding window analysis, for Peromyscus eremicus:
-angsd/misc/thetaStat do_stat angsd_pecr_Win.thetas.idx -win 1000 -step 1000  -outnames angsd_pecr_Win.1kthetasWindow.gz
+angsd/misc/thetaStat do_stat angsd_pecr_Win.thetas.idx -win 1000 -step 1000 -type 2 -outnames angsd_pecr_Win.1kthetasWindow.gz
+  ### type 2 = folded SFS
   ### Use this output to plot Tajima's D and calcuate global average
